26708955982e654bed1e3cd33fc53d03d1230e95
unknown <18482177461@163.com>
2019-02-24 23:56:07 +0800

    连接mysql,post,解析传参

--- DB/DBconfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mysql: {
-        host: 'localhost',
-        user: 'root',
-        password: '123',
-        database: 'my_db'
-    }
-}
-
--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var app = express();
+const connection = require('./modle/mysql');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.get('/user', function (req, res) {
     res.send('hello world');
 });
-app.post('/user/data', function (req, res) {
-    console.log('req:', req)
-    // if(req.params.type==1){
-        res.send(req);
-    // }
+app.get('/user/data', function (req, res) {
+    const data = connection.query('SELECT * FROM users;', function (err, rows, fields) {
+        if (err) console.log(err);
+        res.send(successResponse(rows))
+    })
 });
+
+app.post('/user/data1', function (req, res) {
+    const data = connection.query('SELECT * FROM users;', function (err, rows, fields) {
+        if (err) console.log(err);
+        res.send(successResponse(rows))
+    })
+})
+
+function successResponse(data, msg = '') {
+    return { code: 0, data: data, msg }
+}
+
 app.use(express.static('public'));
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
