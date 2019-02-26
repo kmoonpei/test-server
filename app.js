03006c3689cd68cf220351b387d6685dd4c8802c
Yuki <18482177461@163.com>
2019-02-26 18:28:12 +0800

    登录退出接口

--- app.js
+++ app.js
@@ -6,51 +6,25 @@ var session = require('express-session');
 // var FileStore = require('session-file-store')(session);
 
 // var cookieParser = require('cookie-parser');
-var redis = require('redis');
-var client = redis.createClient('6379', '127.0.0.1');// 默认监听6379端口,'127.0.0.1'为你本地ip(默认不需要修改)
-var RedisStore = require('connect-redis')(session);
+// var redis = require('redis');
+// var client = redis.createClient('6379', '127.0.0.1');// 默认监听6379端口,'127.0.0.1'为你本地ip(默认不需要修改)
+// var RedisStore = require('connect-redis')(session);
 
-var identityKey = 'skey';
+
 app.use(bodyParser.json());
-// app.use(cookieParser());
-
-// redis 链接错误
-client.on_connect('error', function (error) {
-    console.log(error);
-})
 
 // 配置session
 app.use(session({
-    name: identityKey,
+    name: 'skey',
     secret: 'sessiontestlxp',  // 用来对session id相关的cookie进行签名
-    store: new RedisStore({client:client}),  // (使用redis的存储session)
+    // store: new RedisStore({client:client}),  // (使用redis的存储session)
+    // store: new FileStore(),  // 本地存储session（文本文件，也可以选择其他store，比如redis的）
     saveUninitialized: false,  // 是否自动保存未初始化的会话，建议false
     resave: false,  // 是否每次都重新保存会话，建议false
     cookie: {
-        maxAge: 10*60 * 1000  // 有效期，单位是毫秒, 这里设置的是10分钟
+        maxAge: 10 * 60 * 1000  // 有效期，单位是毫秒, 这里设置的是10分钟
     }
 }));
-
-// app.use(session({
-//     name: identityKey,
-//     secret: 'sessiontestlxp',// 用来对session id相关的cookie进行签名
-//     store: new FileStore(),  // 本地存储session（文本文件，也可以选择其他store，比如redis的）
-//     saveUninitialized: false,  // 是否自动保存未初始化的会话，建议false
-//     resave: false,  // 是否每次都重新保存会话，建议false
-//     cookie: {
-//         maxAge: 10 * 1000  // 有效期，单位是毫秒
-//     }
-// }))
-
-// 检测 session是否正常
-// app.use(function (req, res, next) {
-//     if (!req.session) {
-//         return next(new Error('session错误'))
-//     }else {
-//         console.log(req.session)//正常打印当前session
-//     }
-//     next() // 正常 载入下一个中间件
-// })
 
 app.get('/user', function (req, res) {
     res.send('hello world');
@@ -65,43 +39,51 @@ app.post('/user/data1', function (req, res) {
 
 //登录接口
 app.post('/user/login', function (req, res, next) {
-    var sess = req.session;
     if (req.body.name && req.body.pwd) {
         req.session.regenerate(function (err) {
             if (err) {
                 return res.send({ code: 2, msg: '登录失败' });
             }
-            req.session.loginUser = 'lili';
-            res.send(successResponse({}, '登录成功'));
+            req.session.loginUser = req.body.name;
+            res.send(successResponse({ sess: req.session.loginUser }, '登录成功'));
         });
     } else {
         res.send({ code: 1, msg: '账号或密码错误' });
     }
 })
 
-//退出接口
+// 退出登录
 app.get('/user/logout', function (req, res, next) {
-    // 备注：这里用的 session-file-store 在destroy 方法里，并没有销毁cookie
-    // 所以客户端的 cookie 还是存在，导致的问题 --> 退出登陆后，服务端检测到cookie
-    // 然后去查找对应的 session 文件，报错
-    // session-file-store 本身的bug    
-
     req.session.destroy(function (err) {
         if (err) {
-            res.json({ ret_code: 2, ret_msg: '退出登录失败' });
+            res.json({ code: 2, msg: '退出登录失败' });
             return;
         }
 
-        req.session.loginUser = null;
-        res.clearCookie(identityKey);
-        res.redirect('/');
+        // res.clearCookie('skey');
+        // res.redirect('/');
+        res.send(successResponse({}, '退出登录'))
     });
-
-    // res.send(successResponse({}, '请求成功'))
 });
 
+app.post('/user/order_list', function (req, res, next) {
+    var type = req.body.type;
+    if (type === 1) {
+        var loginUser = req.session.loginUser;
+        if (loginUser) {
+            res.send(successResponse({ sid: req.sessionID, loginUser: loginUser }, 'list请求成功'))
+        } else {
+            res.send({ code: 1, msg: '用户还未登录，请先登录' })
+        }
+    } else {
+        res.send({ code: 1, msg: '参数错误' })
+    }
+
+})
+
+
 function successResponse(data, msg = '') {
-    return { code: 0, data: data, msg }
+    return { code: 0, msg, data: data }
 }
 
 app.use(express.static('public'));
